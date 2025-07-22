e2470324eb295452a65218429967fa68b1237d9a
2k1l <katherinelam221@gmail.com>
2025-07-22 00:55:59 -0400

    Updates

--- src/App.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import Beginning from "./lib/Beginning.svelte";
-
-  import Timeline from "./lib/Timeline.svelte";
-</script>
-<Beginning />
-<Timeline />
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-export default defineConfig({
-  base: '/kwkxbwdc-project',    // right here! your repository name
-  plugins: [svelte()],
-})
